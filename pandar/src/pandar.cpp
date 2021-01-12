bc6f71b3cb2ce89ad16b555e22823d0b6f9ccc47
hungkyun <1064776428@qq.com>
2021-01-12 17:12:45 +0800

    edit

--- pandar/src/pandar.cpp
+++ pandar/src/pandar.cpp
@@ -10,6 +10,7 @@ static void main_topic_callback(const sensor_msgs::PointCloud2::ConstPtr& input)
     msg.header.frame_id = "velodyne";
     g_scan_pub.publish(msg);
     std::cout<<"ok!"<<std::endl;
+    std::cout<<"ok!"<<std::endl;
 }
 int main(int argc, char *argv[])
 {
